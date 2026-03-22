85c4f0bcd2435eda5d4f04d32d2d24057f3b0697
DexTher22 <markmagyar2003@gmail.com>
2026-03-22 13:56:45 +0100

    kész

--- src/app.js
+++ src/app.js
@@ -1,4 +1,37 @@
 const form = document.querySelector('#form')
 const weightInput = document.querySelector('#weight')
 const heightInput = document.querySelector('#height')
+const bmiIndex = document.querySelector('#bmiIndex')
 
+form.addEventListener('submit', (event) => {
+    event.preventDefault()
+    console.log('Szamit..')
+    bmiIndex.value = calculate()
+    result = bmiIndex.value
+    tableGrey()
+    tableGreenRow(result)
+})
+
+function calculate() {
+    const weight = Number(weightInput.value)
+    const height = Number(heightInput.value / 100)
+    const result = weight / (Math.pow(height,2))
+    return result
+}
+
+function tableGrey() {
+    document.querySelectorAll('tbody tr').forEach(row => {
+        row.classList.remove('table-success')
+        
+    });
+}
+
+function tableGreenRow(bmi){
+    document.querySelectorAll('tbody tr').forEach(row => {
+        const min = parseFloat(row.dataset.min)
+        const max = parseFloat(row.dataset.max)
+        if (bmi >= min && bmi <= max) {
+        row.classList.add('table-success')
+        }
+    })
+}
